9b8e2e9b5b4eb3f66c4fbcd9ade5680b0c11c128
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-26 12:31:28 -0400

    initial code

---
+++ src/url-to-sha-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* global describe, it */
+const urlToSha = require('.')
+
+describe('url-to-sha', () => {
+  it('write this test', () => {
+    console.assert(urlToSha, 'should export something')
+  })
+})
