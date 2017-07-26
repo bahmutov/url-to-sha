470924d65674a36ae9b8613f7c506a4f4df70f5d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-26 12:36:42 -0400

    copied code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-'use strict'
+const got = require('got')
+const la = require('lazy-ass')
+const is = require('check-more-types')
+const debug = require('debug')('url-to-sha')
 
+module.exports = (pluginConfig, config, cb) => {
+  la(is.url(pluginConfig.url), 'missing url in the plugin config', pluginConfig)
+  debug('plugin config %j', pluginConfig)
 
-module.exports = true
-
+  const property = pluginConfig.property || 'id'
+  got(pluginConfig.url, {json: true})
+    .then(r => r.body)
+    .then(build => build[property])
+    .then(sha => {
+      debug('build property "%s" has SHA %s', property, sha)
+      cb(null, {gitHead: sha})
+    })
+}
