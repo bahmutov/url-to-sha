c6e898eb4554e2d5d8546bf9c2a9e1971e1d1faa
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-28 09:42:25 -0400

    fix(test): add tests, close #1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ const la = require('lazy-ass')
 const is = require('check-more-types')
 const debug = require('debug')('url-to-sha')
 
+function tap (fn) {
+  return function (value) {
+    fn(value)
+    return value
+  }
+}
+
+function printObject (object) {
+  debug('fetched object')
+  debug(object)
+}
+
 module.exports = (pluginConfig, config, cb) => {
   la(is.url(pluginConfig.url), 'missing url in the plugin config', pluginConfig)
   debug('plugin config %j', pluginConfig)
@@ -10,6 +22,7 @@ module.exports = (pluginConfig, config, cb) => {
   const property = pluginConfig.property || 'id'
   got(pluginConfig.url, { json: true })
     .then(r => r.body)
+    .then(tap(printObject))
     .then(build => build[property])
     .then(sha => {
       debug('build property "%s" has SHA %s', property, sha)
--- src/url-to-sha-spec.js
+++ src/url-to-sha-spec.js
@@ -1,10 +1,29 @@
 'use strict'
 
-/* global describe, it */
+/* eslint-env mocha */
 const urlToSha = require('.')
+const nock = require('nock')
+const la = require('lazy-ass')
+const snapshot = require('snap-shot')
 
 describe('url-to-sha', () => {
-  it('write this test', () => {
-    console.assert(urlToSha, 'should export something')
+  const json = {
+    id: 'foo'
+  }
+  beforeEach(() => {
+    nock('https://fake-domain.com').get('/build.json').reply(200, json)
+  })
+
+  it('returns SHA', done => {
+    const pluginConfig = {
+      url: 'https://fake-domain.com/build.json'
+    }
+    const cb = (err, sha) => {
+      console.log('err', err, 'sha', sha)
+      la(err === null, 'got an error', err)
+      snapshot(sha)
+      done()
+    }
+    urlToSha(pluginConfig, null, cb)
   })
 })
